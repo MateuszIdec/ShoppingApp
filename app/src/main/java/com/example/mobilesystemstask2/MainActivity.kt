6f4a56079c4050284131a22aaf0cb17632d2df46
Mateuszidec <midec.p@gmail.com>
2022-11-29 21:38:53 +0100

    Add line chart

--- app/src/main/java/com/example/mobilesystemstask2/MainActivity.kt
+++ app/src/main/java/com/example/mobilesystemstask2/MainActivity.kt
@@ -1,8 +1,5 @@
 package com.example.mobilesystemstask2
 
-
-
-import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -11,30 +8,20 @@ import com.example.mobilesystemstask2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var sensorManager: SensorManager
-    private val diceMap = mapOf(1 to "one", 2 to "two", 3 to "three", 4 to "four", 5 to "five", 6 to "six")
-    private var currentView = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        val button = findViewById<Button>(R.id.button)
 
 
-
-//        button.setOnClickListener {
-//            rollDice(currentView)
-//        }
         binding.bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.one_menu -> {
                     replaceFragment(FirstFragment())
-                    currentView = 1
                 }
                 R.id.two_menu -> {
                     replaceFragment(SecondFragment())
-                    currentView = 2
                 }
             }
             return@setOnItemSelectedListener true
--- app/src/main/java/com/example/mobilesystemstask2/SecondFragment.kt
+++ app/src/main/java/com/example/mobilesystemstask2/SecondFragment.kt
@@ -1,12 +1,19 @@
 package com.example.mobilesystemstask2
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.example.mobilesystemstask2.databinding.FragmentSecondBinding
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import kotlin.collections.ArrayList
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,27 +21,59 @@ import com.example.mobilesystemstask2.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
-
+    private var chart: LineChart? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        savedInstanceState: Bundle?): View {
+        _binding =  FragmentSecondBinding.inflate(inflater, container, false)
+        chart = binding.chart
+        setLineChartData()
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun setLineChartData() {
+
+        val lineDataSet = LineDataSet(exampleData(), "First shop")
+        lineDataSet.color = Color.GRAY
+        val data = LineData(lineDataSet)
+        lineDataSet.circleRadius = 4f
+        lineDataSet.setCircleColor(Color.BLACK)
+        lineDataSet.lineWidth = 2f
+
+        val xAxis = chart!!.xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        val yAxis = chart!!.axisLeft
+        yAxis.spaceTop = 40f
+        yAxis.labelCount = 4
+
+        chart!!.description.isEnabled = false
+        chart!!.axisRight.setDrawGridLines(false)
+        chart!!.xAxis.setDrawGridLines(false)
+        chart!!.axisRight.setDrawLabels(false)
+        chart?.xAxis?.granularity = 0.5f
+        chart?.data = data
+        chart?.setVisibleXRangeMaximum(4f)
+        chart?.invalidate()
+    }
+
+    private fun exampleData() : ArrayList<Entry> {
+        val dataValues = ArrayList<Entry>()
+        dataValues.add(Entry(0f,2.2f))
+        dataValues.add(Entry(1f,2.6f))
+        dataValues.add(Entry(2f,2.3f))
+        return dataValues
+    }
+
 }
